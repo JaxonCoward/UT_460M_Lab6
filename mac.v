4b5576bbf32bffa7e262772cfe0c218908b10957
JaxonCoward <jaxon@DESKTOP-GSOR9ME.localdomain>
2021-04-15 23:32:40 -0500

    Mac module skeleton

---
+++ mac.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/15/2021 11:30:45 PM
+// Design Name: 
+// Module Name: mac
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mac(
+    input Clk,
+    input [7:0] Ain,
+    input [7:0] B,
+    input [7:0] C,
+    input Load,
+    output [7:0] Aout
+    );
+endmodule
