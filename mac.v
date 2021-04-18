2365c4aaf036ef7f6cb593d85bccaa22034dc8aa
JaxonCoward <jaxon@DESKTOP-GSOR9ME.localdomain>
2021-04-17 20:15:05 -0500

    Mac without addition, error in exponent

--- mac.v
+++ mac.v
@@ -33,22 +33,50 @@ module mac(
 
     reg [2:0] cs = 0;
     reg [2:0] ns = 0;
+    reg stop;
 
-    always @(posedge clk) begin
-        cs <= ns;
+    wire [3:0] frac_wire;
 
+    reg [2:0]expB;
+    reg [2:0]expC;
+
+    reg [10:0] product;
+    reg [3:0] exp_adder;
+    reg sign_bit;
+
+    calc_frac_bits fraction(product, frac_wire);
+    
+
+    assign Aout = {sign_bit, exp_adder[2:0], frac_wire};
+    assign Done = stop;
+
+    always @(*) begin
         case(cs)
         0: begin
-            
+            stop <= 1;
+
+            if(Load) ns <= 1;
+            else ns <= 0;
             end
         1: begin 
-            
+            stop <= 0;
+            if(B == 0 || C== 0) product <= 0;
+            else product <= {7'h1, B[3:0]} * {7'h1, C[3:0]};
+
+            expB <= B[6:4]- 3;
+            expC <= C[6:4]- 3;
+            sign_bit <= B[7] ^ C[7];
+
+            ns <= 2;
             end
         2: begin 
-             
+            ns <= 0;
+
+            exp_adder <= expB + expC + 3;
+
             end
         3: begin 
-            
+            ns <= 0;
             end
          4: begin 
             
@@ -57,6 +85,10 @@ module mac(
             ns <= 0;
             end
         endcase
+    end
+
+    always @(posedge Clk) begin
+        cs <= ns;
     end
 
 
---
+++ mac_test.do
@@ -0,0 +1,52 @@
+force Clk 0 0ns, 1 5ns -repeat 10ns
+force {C[7]} 0
+force {C[6]} 0
+force {C[5]} 1
+force {C[4]} 1
+force {C[3]} 1
+force {C[2]} 0
+force {C[1]} 0
+force {C[0]} 0
+
+force {B[7]} 0
+force {B[6]} 0
+force {B[5]} 1
+force {B[4]} 0
+force {B[3]} 1
+force {B[2]} 0
+force {B[1]} 0
+force {B[0]} 0
+
+force Load 1
+
+run 20 ns
+
+force Load 0
+
+run 40 ns
+
+force {C[7]} 1
+force {C[6]} 1
+force {C[5]} 1
+force {C[4]} 1
+force {C[3]} 0
+force {C[2]} 1
+force {C[1]} 0
+force {C[0]} 0
+
+force {B[7]} 0
+force {B[6]} 0
+force {B[5]} 1
+force {B[4]} 0
+force {B[3]} 1
+force {B[2]} 0
+force {B[1]} 0
+force {B[0]} 0
+
+force Load 1
+
+run 20 ns
+
+force Load 0
+
+run 40 ns
